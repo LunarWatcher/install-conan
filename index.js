5c0b4f58194eedf77ca0dce249747fe77ba91742
Olivia Zoe <zoe.i2k1@gmail.com>
2020-05-22 16:07:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const exec = require("child_process").exec;
  * Wrapper function to execute a system command
  */
 function executeCommand(command) {
-    exec (command, 
+    return exec (command, 
         function(error, stdout, stderr) {
             console.log(stdout);
             console.log("stderr: " + stderr);
@@ -16,16 +16,21 @@ function executeCommand(command) {
 }
 
 // Install Conan
-executeCommand("python -m pip install conan");
+let py = executeCommand("python -m pip install conan");
 
-// Possibly unsupported on Windows
-let CXX = core.getInput("CXX");
-let CC = core.getInput("CC");
+// exec is async. Without this,
+// conan will execute in parallel with the install, 
+// which is relatively slow.
+py.on("exit", () => {
+    // Possibly unsupported on Windows
+    let CXX = core.getInput("CXX");
+    let CC = core.getInput("CC");
 
-// Generate the conan profile command
-let base = "conan profile new --detect default"
-let command = 
-        (CXX == "" ? "" : "CXX=" + CXX + " ")
-        + (CC == "" ? "" : "CC=" + CC + " ")
-        + base;
-executeCommand(command);
+    // Generate the conan profile command
+    let base = "conan profile new --detect default"
+    let command = 
+            (CXX == "" ? "" : "CXX=" + CXX + " ")
+            + (CC == "" ? "" : "CC=" + CC + " ")
+            + base;
+    executeCommand(command);
+});
